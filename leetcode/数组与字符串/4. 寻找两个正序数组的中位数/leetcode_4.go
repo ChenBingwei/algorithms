8e114aa6014b6b65803dafb830fcc3867103600b
chenbw <opt_cbw@hotmail.com>
2021-08-07 20:04:11 +0800

    leetcode 4 python

--- leetcode/数组与字符串/4. 寻找两个正序数组的中位数/leetcode_4.go
+++ leetcode/数组与字符串/4. 寻找两个正序数组的中位数/leetcode_4.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func findMedianSortedArrays(nums1 []int, nums2 []int) (ans float64) {
 	m, n := len(nums1), len(nums2)
@@ -27,9 +30,87 @@ func findMedianSortedArrays(nums1 []int, nums2 []int) (ans float64) {
 	return
 }
 
+func findMedianSortedArrays_p(nums1 []int, nums2 []int) (ans float64) {
+	if len(nums1) > len(nums2) {
+		nums1, nums2 = nums2, nums1
+	}
+	m, n := len(nums1), len(nums2)
+
+	// 分割线左边的元素需要满足的个数
+	// 由于是整数除向下取整，在约定total为奇数时左边做一个的情况，
+	// 分割线左边的数量均可以表示为如下
+	totalLeft := (m + n + 1) / 2
+
+	// 在nums1的区间[0,m]里查找恰当的分割线，
+	// 假定i,j分别为nums1和Nums2在分割线右边元素的下标，同时也是nums1和nums2在分割线左边元素个数
+	// 使得 nums1[i-1]<=nums2[j] && nums1[i] >= nums2[j-1]
+	left := 0
+	right := m
+	for {
+		if left >= right {
+			break
+		}
+		i := left + (right-left+1)/2
+		j := totalLeft - i
+		if nums1[i-1] > nums2[j] {
+			// 下一轮搜索区间 [left, i - 1]
+			right = i - 1
+		} else {
+			// 下一轮搜索区间 [i, right]
+			// [left(i), right]
+			left = i
+		}
+	}
+	i := left
+	j := totalLeft - i
+	var nums1LeftMax, nums1RightMin, nums2LeftMax, nums2RightMin int
+	if i == 0 {
+		nums1LeftMax = math.MinInt32
+	} else {
+		nums1LeftMax = nums1[i-1]
+	}
+	if i == m {
+		nums1RightMin = math.MaxInt32
+	} else {
+		nums1RightMin = nums1[i]
+	}
+	if j == 0 {
+		nums2LeftMax = math.MinInt32
+	} else {
+		nums2LeftMax = nums2[j-1]
+	}
+	if j == n {
+		nums2RightMin = math.MaxInt32
+	} else {
+		nums2RightMin = nums2[j]
+	}
+
+	if (m+n)%2 == 1 {
+		ans = float64(max(nums1LeftMax, nums2LeftMax))
+	} else {
+		ans = float64(max(nums1LeftMax, nums2LeftMax)+min(nums1RightMin, nums2RightMin)) / 2
+	}
+	return
+
+}
+
+func max(x int, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
+
+func min(x int, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
+
 func main() {
 	res1 := findMedianSortedArrays([]int{1, 3}, []int{2})
 	fmt.Println(res1)
-	res2 := findMedianSortedArrays([]int{1, 2}, []int{3, 4})
+	res2 := findMedianSortedArrays_p([]int{1, 2}, []int{3, 4})
 	fmt.Println(res2)
 }
--- leetcode/数组与字符串/4. 寻找两个正序数组的中位数/leetcode_4.py
+++ leetcode/数组与字符串/4. 寻找两个正序数组的中位数/leetcode_4.py
@@ -1,3 +1,4 @@
+import sys
 from typing import List
 
 
@@ -24,7 +25,37 @@ class Solution:
         ans = float((left_median + right_median) / 2)
         return ans
 
+    @staticmethod
+    def findMedianSortedArrays_p(nums1: List[int], nums2: List[int]) -> float:
+        if len(nums1) > len(nums2):
+            nums1, nums2 = nums2, nums1
+        m, n = len(nums1), len(nums2)
+        total_left = (m + n + 1) // 2
+        left = 0
+        right = m
+        while left < right:
+            i = left + (right - left + 1) // 2
+            j = total_left - i
+            if nums1[i - 1] > nums2[j]:
+                right = i - 1
+            else:
+                left = i
+        i_final = left
+        j_final = total_left - i_final
+
+        nums1_left_max = -sys.maxsize if i_final == 0 else nums1[i_final - 1]
+        nums1_right_min = sys.maxsize if i_final == m else nums1[i_final]
+        nums2_left_max = -sys.maxsize if j_final == 0 else nums2[j_final - 1]
+        nums2_right_min = sys.maxsize if j_final == n else nums2[j_final]
+
+        if (m + n) % 2 == 1:
+            return float(max(nums1_left_max, nums2_left_max))
+        else:
+            return float(max(nums1_left_max, nums2_left_max) + min(nums2_right_min, nums1_right_min)) / 2
+
 
 if __name__ == '__main__':
     res1 = Solution.findMedianSortedArrays([1, 3], [2])
     print(res1)
+    res2 = Solution.findMedianSortedArrays_p([], [1])
+    print(res2)
