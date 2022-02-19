e65f67166279f9a9d8e166c1d52c87c300f8945f
chenbw <opt_cbw@hotmail.com>
2022-02-19 12:39:52 +0800

    leetcode 69 ini python

---
+++ leetcode/69. X的平方根/leetcod_69.py
@@ -0,0 +1,17 @@
+def mySqrt(x: int) -> int:
+    if x in [0, 1]:
+        return x
+
+    low, high, res = 1, x, -1
+    while low <= high:
+        mid = (low + high) // 2
+        if mid * mid <= x:
+            res = mid
+            low = mid + 1
+        else:
+            high = mid - 1
+    return res
+
+
+if __name__ == '__main__':
+    print(mySqrt(8))
